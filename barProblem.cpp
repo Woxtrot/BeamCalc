ea69f61b7df5ba81ee052b8eb0398f834aecce9d
Woxtrot <Woxtrot@users.noreply.github.com>
2023-10-30 21:38:56 +0100

    additional comments

--- barProblem.cpp
+++ barProblem.cpp
@@ -13,21 +13,21 @@ using std::vector;
 using std::ofstream;
 
 // Derived class for a bar problem
-void barProblem::solve() {
+void barProblem::solve() { // Prompt to indicate that code has reached calculation
     cout << "Solving a bar problem....." << endl;
-    vector<double> positions, normalLoads, deflections, stress;
-    if (supportType == 1){
-        if (loadType == 1){}
-        if (loadType == 2){}
+    vector<double> positions, normalLoads, deflections, stress; // declaring vectors used for the results
+    if (supportType == 1){ //cantilever
+        if (loadType == 1){} //point load
+        if (loadType == 2){} //distributed load
     }
 
     // Export the data to a file
-    ofstream outFile("bar_data.csv");
-    outFile << "Position (m), Deflection (m), Normal Force (N), Normal Stress (Pa)\n";
-    for (size_t i = 0; i < positions.size(); i++) {
+    ofstream outFile("bar_data.csv"); // exporting to csv file as it is easy to import into for example excel
+    outFile << "Position (m), Deflection (m), Normal Force (N), Normal Stress (Pa)\n"; // column names
+    for (size_t i = 0; i < positions.size(); i++) { //for every entry of the vectors, value is printed for different results and separated between commas
         outFile << positions[i] << "," << deflections[i] << ","  << normalLoads[i] << "," << stress[i] << "\n";
     }
-    outFile.close();
+    outFile.close(); // closing of the file
 
 }
 
--- shaftProblem.cpp
+++ shaftProblem.cpp
@@ -15,20 +15,20 @@ using std::ofstream;
 // Derived class for a shaft problem
 void shaftProblem::solve() {
     double G = modulus; // Changing variable for ease of calculation.
-    cout << "Solving a shaft problem....." << endl;
+    cout << "Solving a shaft problem....." << endl; // Prompt to indicate that code has reached calculation
     vector<double> positions, torsionLoads, deflections, stress;
-    if (supportType == 1){
-        if (loadType == 1){}
-        if (loadType == 2){}
+    if (supportType == 1){ // cantilever
+        if (loadType == 1){} //point load
+        if (loadType == 2){} //distributed load
     }
 
     // Export the data to a file
     ofstream outFile("shaft_data.csv");
-    outFile << "Position (m), Angle of Twist (radians), Torsion (Nm), Torsion Stress (Pa)\n";
-    for (size_t i = 0; i < positions.size(); i++) {
+    outFile << "Position (m), Angle of Twist (radians), Torsion (Nm), Torsion Stress (Pa)\n"; // column names
+    for (size_t i = 0; i < positions.size(); i++) { //for every entry of the vectors, value is printed for different results and separated between commas
         outFile << positions[i] << "," << deflections[i]  << "," << torsionLoads[i] << "," << stress[i] << "\n";
     }
-    outFile.close();
+    outFile.close(); // closing of the file
 
 }
 
